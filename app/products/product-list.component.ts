7657e530d7cf955397e74a0e8a946e9c53418f35
Rene Escobar <rene@unisonagency.com>
2016-08-09 15:05:13 -0400

    toggle images on and off using event handlers

--- app/products/product-list.component.ts
+++ app/products/product-list.component.ts
@@ -8,6 +8,9 @@ import {Component} from 'angular2/core';
 
 export class ProductListComponent {
     pageTitle: string = 'Product List';
+    imageWidth: number = 50;
+    imageMargin: number = 2;
+    showImage: boolean = false;
     products: any[] = [
         {
             "productId": 1,
@@ -31,6 +34,8 @@ export class ProductListComponent {
         }
     ];
 
-
+    toggleImage(): void {
+        this.showImage = !this.showImage;
+    }
 }
 
